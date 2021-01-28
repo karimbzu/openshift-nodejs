acbae3584f64e0f63e8d1e2e89a9042a00024d0a
karimbzu <ahmadkarimphd@gmail.com>
2021-01-28 21:16:26 +0500

    third change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express()
 var PORT = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-	res.send('Hello World From Mary Openshift!');
+	res.send('Hello World David!');
 });
 
 app.listen(PORT);
